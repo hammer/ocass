92660dd6c70d32f225e832295d3c3175abd47ae6
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2014-10-07 22:52:47 -0400

    Trying some basic memory profiling.

--- using_suffix_trees.ml
+++ using_suffix_trees.ml
@@ -14,6 +14,8 @@ let get_sq embl_file =
 let () =
   let string_to_find = "caat" in
   let string_to_search = get_sq embl_file in
+  Printf.printf "Size of string: %d\n" (Obj.size (Obj.repr (string_to_search)));
   let tree_to_search = Suffix_tree.Ukkonen.create string_to_search in
+  Printf.printf "Size of tree: %d\n" (Obj.size (Obj.repr (tree_to_search))); (* not useful *)
   let position_found = Suffix_tree.Ukkonen.substring tree_to_search string_to_find in
   Printf.printf "Found string %s at position %d in string %s\n" string_to_find position_found string_to_search
