a67c57c1c5f4c4f9d6f27571946b0f2a24f78eb7
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2014-10-14 23:22:13 -0400

    Add another test

--- test_using_z_alg.ml
+++ test_using_z_alg.ml
@@ -2,6 +2,11 @@ open OUnit2
 
 let string1 = "aabcaabxaaz"
 
+let test_prefix_match_length test_ctxt =
+  let text = BatString.to_list string1 in
+  let pos = 4 in
+  assert_equal 3 (Using_z_alg.prefix_match_length text pos)
+
 let test_z_alg test_ctxt =
   let pattern = string1 in
   let expected_zs = [| 0; 1; 0; 0; 3; 1; 0; 0; 2; 1; 0 |] in
@@ -10,7 +15,9 @@ let test_z_alg test_ctxt =
 
 let suite =
   "suite" >:::
-    [ "test_z_alg" >:: test_z_alg ]
+    [ "test_z_alg" >:: test_z_alg;
+      "test_prefix_match_length" >:: test_prefix_match_length
+    ]
 
 let () =
   run_test_tt_main suite
